d12675cf08e1d56135b5d88e292b81a7a6f05125
skwin258 <yy6611345@gmail.com>
2025-09-25 16:43:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ const allowedUsers = new Set([
   'Uc3be515b0b2e4c8807ad8552d40d1714',
   'U66c38a1baa388cccfd060c0206032955',
   'U2031c52d282931d135d54e21c5657184',
+  'U2984dc3b25a935e69f40704cbb6246b6',
 ]);
 
 // ====== 狀態暫存（記憶體版） ======
